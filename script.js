53bd3544f967397c0fa9b5e41e3a37c79c0a1b32
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-30 00:53:17 +0530

    Added the scripts for general dom manipulation...

---
+++ script.js
@@ -0,0 +1,7 @@
+const panel = document.getElementById("panel");
+const sendBtn = document.getElementById("send");
+const rating = document.querySelectorAll(".rating");
+
+panel.addEventListener("click", (e) => {
+  console.log(e.target);
+});
