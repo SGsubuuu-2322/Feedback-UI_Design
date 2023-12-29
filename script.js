4a139e85f68a092b5041a3e3ee042e169f5cb92e
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-30 01:01:35 +0530

    Added the scripts for selection of emojis...

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const panel = document.getElementById("panel");
 const sendBtn = document.getElementById("send");
-const rating = document.querySelectorAll(".rating");
+const ratings = document.querySelectorAll(".rating");
 
 panel.addEventListener("click", (e) => {
-  console.log(e.target);
+  if (e.target.parentNode.classList.contains("rating")) {
+    removeActive();
+    e.target.parentNode.classList.add("active");
+  }
 });
+
+function removeActive() {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove("active");
+  }
+}
