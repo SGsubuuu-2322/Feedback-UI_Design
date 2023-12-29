8eac9aca4c2972e564f2f9bd62bd6a701df56a38
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-30 01:13:18 +0530

    Added the scripts for displaying the feedback message...

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ const panel = document.getElementById("panel");
 const sendBtn = document.getElementById("send");
 const ratings = document.querySelectorAll(".rating");
 
+let selectedRating = "satisfied";
+
 panel.addEventListener("click", (e) => {
   if (e.target.parentNode.classList.contains("rating")) {
     removeActive();
     e.target.parentNode.classList.add("active");
+    selectedRating = e.target.nextElementSibling.innerHTML;
   }
+});
+
+sendBtn.addEventListener("click", () => {
+  panel.innerHTML = `
+
+     <i class="fas fa-heart"></i>
+     <strong>Thank you!</strong>
+     </br>
+     <strong>Feedback: ${selectedRating}</strong>
+     <p>We'll use you feedback to improve our customer support...</p>
+    `;
 });
 
 function removeActive() {
